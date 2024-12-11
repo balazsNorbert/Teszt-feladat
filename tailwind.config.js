23372c7d19d4551f533cf90339280d0bd8cd2f5a
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2024-12-11 12:34:32 +0200

    Navbar for desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lexend: ["Lexend", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      backgroundImage: {
+        'headerImage': "url('/src/assetek/jpg/security-camera-with-glass-building-background.jpg')",
+      },
+      colors: {
+        'orangeCustom': '#EF7C00',
+      },
+      padding: {
+        'navPadding': '14px',
+    },
   },
   plugins: [],
+  }
 }
 
